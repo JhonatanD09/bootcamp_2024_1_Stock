7b2f4110c708f96e9de1da1980ce4fc6077a6b00
JhonatanD09 <JORANADA@GMAIL.COM>
2024-08-21 09:47:26 -0500

    hu 1

---
+++ src/test/java/com/bootcamp_2024_1/Stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_2024_1.Stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
